aa0aa3b2146fb89177058cc4d298b9dc5fd2c1b5
Gil Tolley <tolley.gil@gmail.com>
2018-02-02 10:17:58 -0500

    Cleaned up the UI to seperate sorting and exiting the program, as well as error checking the integer entry.

--- Heap-Sort/Heap/test.cpp
+++ Heap-Sort/Heap/test.cpp
@@ -11,12 +11,17 @@ int main()
 	{
 		cout << "Heap Sort Algorithim" << endl;
 		cout << "Press I to insert an item into the array" << endl;
-		cout << "Press S to sort and exit" << endl;
+		cout << "Press S to sort and display" << endl;
+		cout << "Press X to exit" << endl;
 		cout << "Please enter your choice: ";
 		cin >> choice;
 
 		switch (choice)
 		{
+		case 'X':
+		case 'x':
+			exit = true;
+			break;
 		case 'S':
 		case 's':
 			cout << endl;
@@ -28,13 +33,22 @@ int main()
 			myHeap.printHeap(cout);
 			cout << endl;
 			cout << endl;
-			system("pause");
-			exit = true;
 			break;
 		case 'I':
 		case 'i':
 			cout << "Please enter the item to be inserted: ";
-			cin >> item;
+			//cin >> item;
+			int x;
+			cin >> x;
+			while (cin.fail()) {
+				cout << "Error: Please enter a valid Integer:" << endl;
+				cin.clear();
+				cin.ignore(256, '\n');
+				cin >> x;
+			}
+			cout << x << endl;
+			item = x;
+
 			myHeap.Insert(item);
 			cout << endl;
 			cout << item << " was inserted" << endl;
